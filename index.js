c6dcd5b8ef5e543291af5ebe845bbe47e7c06fca
crishood <crishood13@gmail.com>
2022-08-25 10:37:40 -0500

    Add cors and validator middleware

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 const routerApi = require('./routes');
 
-const { logErrors, errorHandler } = require('./middlewares/error.handler');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middleware/error.handler');
 
 const app = express();
 const port = 3000;
@@ -9,7 +15,11 @@ const port = 3000;
 routerApi(app);
 
 app.use(logErrors);
+app.use(boomErrorHandler);
 app.use(errorHandler);
+
+app.use(cors());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.json({
